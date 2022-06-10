11c76914529407a3d3c739b42fa1adf59ffb182f
мир <106350647+mir3133031@users.noreply.github.com>
2022-06-10 17:01:23 +0500

    Add files via upload

---
+++ scrin.js
@@ -0,0 +1,35 @@
+let names = ["Anna", "Bob", "Charles", "Daniel", "Allison", "Beatrice", "Cindy", "Fiona"];
+
+let book = prompt(`не бльше 5 букв `)
+let  filteredNames = names.filter(function(book) {
+    return book[5] = "j";
+});
+console.log(
+    filteredNames
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let arr = [`jone`, `mirabbos`, `castle`]
+// let filtered = prompt (``)
+// let answer = arr.filter(item => item.toLowerCase().includes(filtered))
+// console.log(answer)
+
+
+
+
+// 1, 2, false, `hello`, 24, `world`, undefined, null, `error`, 22,
